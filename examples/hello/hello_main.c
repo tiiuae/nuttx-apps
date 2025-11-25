0f3ab80d6b6bd2bfb79368398cd2d40328d2192b
Jukka Laitinen <jukka.laitinen@tii.ae>
2025-11-25 10:42:42 +0200

    Replace hello_main.c with delay & sleep tests
    Signed-off-by: Jukka Laitinen <jukka.laitinen@tii.ae>

--- examples/hello/hello_main.c
+++ examples/hello/hello_main.c
@@ -26,6 +26,57 @@
 
 #include <nuttx/config.h>
 #include <stdio.h>
+#include <stdint.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define MPFS_CLINT_BASE      (0x02000000UL)
+#define MPFS_CLINT_MTIME     (MPFS_CLINT_BASE + 0xbff8)
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+static uint64_t cntfrq;
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+#ifdef CONFIG_ARCH_ARM64
+
+static inline uint64_t arch_timer_get_cntfrq(void)
+{
+  return read_sysreg(cntfrq_el0);
+}
+
+static inline uint64_t arch_timer_count(void)
+{
+  return read_sysreg(cntvct_el0);
+}
+
+#elif CONFIG_ARCH_CHIP_MPFS
+
+static inline uint64_t arch_timer_get_cntfrq(void)
+{
+  return 1000000;
+}
+
+static inline uint64_t arch_timer_count(void)
+{
+  return *(volatile uint64_t *)MPFS_CLINT_MTIME;
+}
+
+#else
+#error Not supported
+#endif
+
+static inline uint64_t time_usec(void)
+{
+  return 1000000 * arch_timer_count() / cntfrq;
+}
 
 /****************************************************************************
  * Public Functions
@@ -37,6 +88,97 @@
 
 int main(int argc, FAR char *argv[])
 {
-  printf("Hello, World!!\n");
+  const int udelay_times[] =
+    {
+      1, 10, 100
+    };
+
+  int i;
+  irqstate_t flags;
+  uint64_t t1;
+  uint64_t t2;
+
+  bool errors = false;
+
+  cntfrq = arch_timer_get_cntfrq();
+  DEBUGASSERT(cntfrq != 0);
+
+  usleep(1000);
+
+  /* Comment out the LOOPSMERMSEC sanity check below if "udelay_coarse" is
+   * not in use
+   */
+
+#if 1
+  printf("Calculating expected CONFIG_BOARD_LOOPSPERMSEC for udelay_coarse"
+         "(this will take ~10s)\n");
+  sleep(1);
+
+  /* Coarse LOOPSPERMSEC calibration */
+
+  flags = enter_critical_section();
+  t1 = time_usec();
+  up_udelay(10000000); /* 10s busyloop */
+  t2 = time_usec();
+  leave_critical_section(flags);
+
+  printf("CONFIG_BOARD_LOOPSPERMSEC should be approximately %" PRIu64
+         " and is %d\n", (uint64_t)10000000 * CONFIG_BOARD_LOOPSPERMSEC /
+         (t2 - t1), CONFIG_BOARD_LOOPSPERMSEC);
+#endif
+
+  printf("udelay test\n");
+  for (i = 0; i < sizeof(udelay_times) / sizeof(udelay_times[0]); i++)
+    {
+      /* Enter critical section for accurate measurement of udelay; no
+       * interrupts allowed
+       */
+
+      flags = enter_critical_section();
+
+      t1 = time_usec();
+      up_udelay(udelay_times[i]);
+      t2 = time_usec();
+
+      /* Allow +10us error */
+
+      if (t2 - t1 > udelay_times[i] + 10 || t2 - t1 < udelay_times[i])
+        {
+          printf("ERR: Delay lasted %" PRIu64 " instead of %d\n", t2 - t1,
+                 udelay_times[i]);
+          errors = true;
+        }
+
+      leave_critical_section(flags);
+    }
+
+  printf("udelay test %s\n", errors ? "FAILED" : "PASSED");
+
+  printf ("usleep test; this will take approx. 1 hour\n");
+
+  errors = false;
+  for (i = 0; i < 60 * 60 * (1000000 / CONFIG_USEC_PER_TICK); i++)
+    {
+      flags = enter_critical_section();
+      t1 = time_usec();
+      usleep(CONFIG_USEC_PER_TICK);
+      t2 = time_usec();
+      leave_critical_section(flags);
+
+      /* Allow sleeps + 2 ticks, but never shorter than 1 */
+
+      if (t2 - t1 < CONFIG_USEC_PER_TICK ||
+          t2 - t1 > 3 * CONFIG_USEC_PER_TICK)
+        {
+          errors = true;
+          printf("ERR: Sleep lasted %" PRIu64 "us instead of %d-%dus\n",
+                 t2 - t1, CONFIG_USEC_PER_TICK, 3 * CONFIG_USEC_PER_TICK);
+        }
+
+      /* printf("%" PRIu64 " %" PRIu64 " %" PRIu64 "\n", t1, t2, t2 - t1); */
+    }
+
+  printf("usleep test %s\n", errors ? "FAILED" : "PASSED");
+
   return 0;
 }
